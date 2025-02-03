a8ebfdc04974b255c53e08e408e2544d3cd17769
Morgan Doane <morganddoane@gmail.com>
2025-02-02 19:54:01 -0500

    profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import Button from '@/components/Button';
 import Tag from './tag';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 
 export default function Home() {
 	return (
 		<div className="h-[80vh] bg-neutral-200 p-6 pt-20 flex items-center justify-center rounded-br-4xl rounded-bl-4xl">
 			<div className="max-w-[1200px]">
+				<Image
+					src="/images/profile.jpg"
+					style={{ height: '120px', width: '120px' }}
+					height={120}
+					width={120}
+					alt="Morgan Doane"
+					className="rounded-full object-cover"
+				/>
 				<p className="text-3xl sm:text-4xl md:text-5xl mb-3 whitespace-pre-line leading-normal text-neutral-800">
 					{"I'm Morgan Doane"}, a{' '}
 					<Tag
